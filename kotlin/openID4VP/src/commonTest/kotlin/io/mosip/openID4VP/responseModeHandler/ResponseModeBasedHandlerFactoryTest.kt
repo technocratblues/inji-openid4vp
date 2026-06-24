3345cfb080f00dd50bdd5405c4b61209cc616e71
Amit Yadav <amit.uitgeek@gmail.com>
2026-06-24 15:08:44 +0530

    Address PR review comments

--- kotlin/openID4VP/src/commonTest/kotlin/io/mosip/openID4VP/responseModeHandler/ResponseModeBasedHandlerFactoryTest.kt
+++ kotlin/openID4VP/src/commonTest/kotlin/io/mosip/openID4VP/responseModeHandler/ResponseModeBasedHandlerFactoryTest.kt
@@ -24,6 +24,7 @@ class ResponseModeBasedHandlerFactoryTest {
         assertNotNull(handler)
     }
 
+
     @Test
     fun `get should return DirectPostJwtResponseModeHandler for direct_post_jwt mode`() {
         val handler = ResponseModeBasedHandlerFactory.get(ResponseMode.DIRECT_POST_JWT.value)
@@ -34,11 +35,19 @@ class ResponseModeBasedHandlerFactoryTest {
 
     @Test
     fun `get should return DirectPostJwtResponseModeHandler for iar_post_jwt mode`() {
-        val handler = ResponseModeBasedHandlerFactory.get(ResponseMode.IAE_POST_JWT.value)
+        val handler = ResponseModeBasedHandlerFactory.get(ResponseMode.IAR_POST_JWT.value)
 
         assertTrue(handler is DirectPostJwtResponseModeHandler)
         assertNotNull(handler)
     }
+
+@Test
+fun `get should return DirectPostJwtResponseModeHandler for iae_post_jwt mode`() {
+    val handler = ResponseModeBasedHandlerFactory.get(ResponseMode.IAE_POST_JWT.value)
+
+    assertTrue(handler is DirectPostJwtResponseModeHandler)
+    assertNotNull(handler)
+}
 
     @Test
     fun `get should return new instances each time for direct_post mode`() {
